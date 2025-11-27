5056fce7c2b1fa8d8b6d2b8e245b2a1948728984
Md Mahammud Ali <ali_mahammud@yahoo.com>
2025-11-27 22:57:33 +0600

    Initial commit

---
+++ main.py
@@ -0,0 +1,18 @@
+# This is a sample Python script.
+
+# Press Ctrl+F5 to execute it or replace it with your code.
+# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+
+
+def print_hi(name):
+    # Use a breakpoint in the code line below to debug your script.
+    print(f'Hi, {name}')  # Press F9 to toggle the breakpoint.
+
+def addition(num1, num2):
+    return print('addition those number', num1 + num2)
+# Press the green button in the gutter to run the script.
+if __name__ == '__main__':
+    print_hi('PyCharm')
+    addition(1, 2)
+
+# See PyCharm help at https://www.jetbrains.com/help/pycharm/
